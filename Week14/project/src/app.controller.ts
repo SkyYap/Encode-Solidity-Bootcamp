3ae631e841886cabdad36d82177ab013baf7a90d
SkyYap <32660903+SkyYap@users.noreply.github.com>
2023-05-18 22:06:05 +0800

    Push

--- Week14/project/src/app.controller.ts
+++ Week14/project/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ethers } from 'ethers';
 
@@ -11,8 +11,29 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Get("last-block")
+  @Get('last-block')
   getLastBlock(): Promise<ethers.providers.Block> {
     return this.appService.getLastBlock();
   }
+
+  @Get('contract-address')
+  getAddress() {
+    return this.appService.getAddress();
+  }
+
+  @Get('total-supply')
+  getTotalSupply() {
+    return this.appService.getTotalSupply();
+  }
+
+  @Get('balance/:address')
+  getBalanceOf(@Param('address') address: string) {
+    return this.appService.getBalanceOf(address);
+  }
+
+  @Get('transaction-receipt')
+  async getTransactionReceipt(@Query('hash') hash: string) {
+    return await this.appService.getTransactionReceipt(hash);
+  }
+
 }
--- Week14/project/src/app.service.ts
+++ Week14/project/src/app.service.ts
@@ -1,13 +1,45 @@
 import { Injectable } from '@nestjs/common';
-import { ethers } from 'ethers';
+import { Contract, ethers } from 'ethers';
+import * as tokenJson from './assets/MyToken.json';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class AppService {
+  provider: ethers.providers.BaseProvider;
+  contract: ethers.Contract;
+
+  constructor(private configService: ConfigService) {
+    this.provider = ethers.getDefaultProvider('sepolia')
+    this.contract = new ethers.Contract(this.getAddress(), tokenJson.abi, this.provider)
+  }
   getHello(): string {
     return 'Hello World!';
   }
-  getLastBlock(): Promise<ethers.providers.Block> {
-    const provider = ethers.getDefaultProvider('goerli')
-    return provider.getBlock("latest")
+
+  getLastBlock() {
+    return this.provider.getBlock("latest")
+  }
+
+  getAddress() {
+    const tokenAddress = this.configService.get<string>('TokenAddress');
+    return '0xaaa84599036017e4f44b506510f4847d5b46471d';
+  }
+
+  getTotalSupply() {
+    return this.contract.getTotalSupply();
+  }
+
+  getBalanceOf(address: string) {
+    return this.contract.balanceOf(address);
+  }
+
+  async getTransactionReceipt(hash: string) {
+    const tx = await this.provider.getTransaction(hash);
+    const receipt = await tx.wait();
+    return receipt
+  }
+
+  async getReceipt(tx: ethers.providers.TransactionResponse) {
+    return await tx.wait();
   }
 }
